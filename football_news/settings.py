ed053b080614633ed123680bb254150cab0d55c6
raihana <zakiaraihanauni@gmail.com>
2025-09-10 14:04:59 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raihana-auni-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "raihana-auni-footballnews.pbp.cs.ui.ac.id"
+    "https://raihana-auni-footballnews.pbp.cs.ui.ac.id/"
 ]
 
 
