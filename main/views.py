18b5f82f0b9335301adfca260f2284a0e133f4be
raihana <zakiaraihanauni@gmail.com>
2025-10-01 13:53:57 +0700

    tutorial 5 selesai: edit views.py

--- main/views.py
+++ main/views.py
@@ -11,6 +11,7 @@ from django.urls import reverse
 import datetime
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
+from django.utils.html import strip_tags
 
 @login_required(login_url='/login')
 def show_main(request):
